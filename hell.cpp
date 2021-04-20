e9277186997f39558e016156894b31dbd3bb9ba8
omkarmohanty36 <omkarmohanty36@gmail.com>
2021-04-20 15:12:15 +0530

    Added to "New" branch

--- hell.cpp
+++ hell.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   cout << "Hell is here, you don't have to go anywhere.";
+  int a;
+  cin >> a;
+
+  cout << "hey added an integer.";
   return 0;
 }
